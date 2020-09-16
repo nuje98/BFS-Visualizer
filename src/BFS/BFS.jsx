c0f5a0d599a27c14007d85710ca365f5e1dde46d
Anuj Jhamb <anuj.jhamb23@gmail.com>
2020-09-17 00:25:49 +0530

    second

--- src/BFS/BFS.jsx
+++ src/BFS/BFS.jsx
@@ -1,3 +1,17 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import '/BFS.css';
+import './BFS.css';
+
+export default class BFS extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="BFS">
+                <canvas></canvas>
+            </div>
+        )
+    }
+}
